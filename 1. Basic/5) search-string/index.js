ade8b5ffe0acd61946f66d501de61abc8581f473
ankopylova <akopylova.excessteam@gmail.com>
2020-11-12 16:33:37 +0300

    /search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,7 +1,13 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+    let str = ""
+    for (var key in obj) {
+        const value = obj[key]
+        if (value !== undefined && value !== null && value !== '') {
+            str += (str.length ? "&" : "?") + key + "=" + value;
+        }
+    }
+    return str
 }
 
 window.searchString = searchString;
-
 export default searchString;

f73c5b71247624dcadc618605f2bff1d2c247961
ankopylova <akopylova.excessteam@gmail.com>
2020-11-13 18:33:11 +0300

    /is-equal-simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,7 +1,16 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+    if (a!==null&&b!==null&&a!==undefined&&b!==undefined){
+        let str,
+            str1;
+  for (let key in a) {
+   str += key + JSON.stringify(a);
+  }
+  for (let key1 in b) {
+   str1 += key1 + JSON.stringify(b);
+  }
+ return (str === str1)
+    }
+  else return a === b;
 }
-
 window.isEqual = isEqual;
-
 export default isEqual;

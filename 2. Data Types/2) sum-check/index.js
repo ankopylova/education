765e9b13ce3ce9d4b2848301e50ab2eb10587c62
ankopylova <akopylova.excessteam@gmail.com>
2020-11-13 18:31:14 +0300

    /sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,7 +1,7 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+        if (typeof a === 'number' && typeof b === 'number') return a+b;
+        else throw new Error('Wrong arguments type!');
 }
 
 window.sumCheck = sumCheck;
-
 export default sumCheck;

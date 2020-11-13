42c4cd8844bc8b6923207d793cfe29f3bfa83e3a
ankopylova <akopylova.excessteam@gmail.com>
2020-11-13 18:30:32 +0300

    /search-string

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,7 +1,18 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+  switch (str){
+    case "true":
+      return true;
+    case "false":
+      return false;
+    case "null":
+      return null;
+    case "undefined":
+      return undefined
+    default:
+      if (/^\d+$/.test(str)) return Number.parseInt(str);
+  }
+  return str;
+}
 
 window.stringToType = stringToType;
-
 export default stringToType;

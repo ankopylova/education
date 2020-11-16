58db8bf4bc75c2033534c72fbc8132017622f655
ankopylova <akopylova.excessteam@gmail.com>
2020-11-16 16:24:50 +0300

    /find-pair

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,6 +1,11 @@
 function findPair(arr) {
-  // Напиши свой код здесь
-};
+    const newArr = arr.sort()
+    let res;
+    for (let i = 0; i < arr.length; i++) {
+        if (newArr[i] === newArr[i + 1]) res = newArr[i];
+    }
+    return res || null;
+}
 
 window.findPair = findPair;
 

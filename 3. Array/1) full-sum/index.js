0e6fdb4f8ac48fed39404ef46d40a71aeba156ae
ankopylova <akopylova.excessteam@gmail.com>
2020-11-16 14:15:24 +0300

    /is-equal-simple

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,7 +1,11 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    let result = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+        if (arguments[i] === undefined || arguments[i] === true || arguments[i] === false) throw new Error('Wrong arguments type!')
+    }
+    if (typeof result !== 'number') throw new Error('Wrong arguments type!');
+    return result;
 }
-
 window.fullSum = fullSum;
-
 export default fullSum;

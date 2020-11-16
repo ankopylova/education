b407b37514fac8ab5aee5f27dfe04251b6d99ab3
ankopylova <akopylova.excessteam@gmail.com>
2020-11-16 14:35:09 +0300

    /full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,11 +1,8 @@
 function fullSum(...args) {
-    let result = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        result += arguments[i];
-        if (arguments[i] === undefined || arguments[i] === true || arguments[i] === false) throw new Error('Wrong arguments type!')
-    }
-    if (typeof result !== 'number') throw new Error('Wrong arguments type!');
-    return result;
+    return args.reduce(function(result, num) {
+        if (typeof num !== 'number') throw new Error('Wrong arguments type!')
+        return result + num;
+    }, 0);
 }
 window.fullSum = fullSum;
 export default fullSum;

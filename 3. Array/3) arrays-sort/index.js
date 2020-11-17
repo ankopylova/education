acddde55250e5b0669a4141544311046aede6d8f
ankopylova <akopylova.excessteam@gmail.com>
2020-11-17 14:41:36 +0300

    /array-sort

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,7 +1,5 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
-};
-
+    return [...arr].sort((a, b) => Math.max(...a) - Math.max(...b));
+}
 window.arraysSort = arraysSort;
-
 export default arraysSort;

0c7efc350dd7ae6f078669417f0cad4dd371d25b
ankopylova <akopylova.excessteam@gmail.com>
2020-11-18 12:03:33 +0300

    /query-object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,7 +1,31 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
+   let obj = {};
+   if (query !== "" && query !== "?") {
+      obj = JSON.parse('{"' + query.split("?")[1].replace
+      (/&/g, '", "').replace
+      (/=/g, '": "') + '"}');
+      for (const key in obj) {
+         obj[key] = stringToType(obj[key]);
+           }
+      }
+   return obj;
+}
+
+function stringToType(str) {
+   switch (str){
+      case "true":
+         return true;
+      case "false":
+         return false;
+      case "null":
+         return null;
+      case "undefined":
+         return undefined
+      default:
+         if (/^\d+$/.test(str)) return Number.parseInt(str)
+         return str;
+   }
+}
 
 window.queryToObject = queryToObject;
-
 export default queryToObject;

0018f8a8e74a15b57a9eabec68d84947dc182c62
ankopylova <akopylova.excessteam@gmail.com>
2020-11-18 17:21:15 +0300

    /query-object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,30 +1,30 @@
 function queryToObject(query) {
-   let obj = {};
-   if (query !== "" && query !== "?") {
-      obj = JSON.parse('{"' + query.split("?")[1].replace
-      (/&/g, '", "').replace
-      (/=/g, '": "') + '"}');
-      for (const key in obj) {
-         obj[key] = stringToType(obj[key]);
-           }
-      }
-   return obj;
+    let obj = {};
+    if (query !== "" && query !== "?") {
+        obj = JSON.parse('{"' + query.split("?")[1].replace
+        (/&/g, '", "').replace
+        (/=/g, '": "') + '"}');
+        for (const key in obj) {
+            obj[key] = stringToType(obj[key]);
+        }
+    }
+    return obj;
 }
 
 function stringToType(str) {
-   switch (str){
-      case "true":
-         return true;
-      case "false":
-         return false;
-      case "null":
-         return null;
-      case "undefined":
-         return undefined
-      default:
-         if (/^\d+$/.test(str)) return Number.parseInt(str)
-         return str;
-   }
+    switch (str) {
+        case "true":
+            return true;
+        case "false":
+            return false;
+        case "null":
+            return null;
+        case "undefined":
+            return undefined
+        default:
+            if (/^\d+$/.test(str)) return Number.parseInt(str)
+            return str;
+    }
 }
 
 window.queryToObject = queryToObject;

9acd62b2e2e3dfaf4d19bbda29f4cbcf35e7f887
ankopylova <akopylova.excessteam@gmail.com>
2020-11-18 16:03:39 +0300

    /async-sum

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,11 +1,11 @@
 function asyncSum(a, b) {
-return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (typeof a === 'number' && typeof b === 'number')
-            resolve(a + b);
-        reject(new Error('error'));
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') resolve(a + b);
+            reject(new Error('error'));
         }, 1);
     });
 }
+
 window.asyncSum = asyncSum;
 export default asyncSum;

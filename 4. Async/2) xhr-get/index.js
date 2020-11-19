92ea605a262d47c0a425f0575a32399175035c29
ankopylova <akopylova.excessteam@gmail.com>
2020-11-19 16:25:51 +0300

    /async-sum

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,7 +1,16 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+    xhr.responseType = "json"
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (xhr.status <= 299) resolve(xhr.response);
+            reject(xhr.response);
+        }, 1);
+    });
 }
 
 window.xhrGet = xhrGet;
-
 export default xhrGet;

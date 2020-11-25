de47c0534ee98c1ddbc235aba2cd41741ae6fa1d
ankopylova <akopylova.excessteam@gmail.com>
2020-11-25 10:16:42 +0300

    /stack

--- 5. Others/6) row-zero/index.js
+++ 5. Others/6) row-zero/index.js
@@ -1,9 +1,7 @@
 function rowZero(str) {
-    let z = str.match(/0+/g);
+    const z = str.match(/0+/g);
     if (z === null) return 0;
-    z.sort();
-    let n = z[z.length - 1];
-    return n.length;
+    return z.sort()[z.length - 1].length;
 }
 
 window.rowZero = rowZero;

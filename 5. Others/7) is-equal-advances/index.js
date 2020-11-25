c72a7d6c2819a7f281e96e7b6b4b06741e00e2b8
ankopylova <akopylova.excessteam@gmail.com>
2020-11-25 11:29:33 +0300

    /is-equal-advanced

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,7 +1,16 @@
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+    if (a === b) return true;
+    if (!a || !b || typeof a !== 'object' || typeof b !== 'object') return a === b;
+    if (a === undefined || b === undefined || a === null || b === null)
+        return a === b
+    if (Object.keys(a).values() === undefined || Object.keys(b).values() === undefined
+        || Object.keys(a).values() === null
+        || Object.keys(b).values() === null) {
+        return Object.keys(a).values() === Object.keys(b).values()
+    }
+    if (Object.keys(a).length !== Object.keys(b).length) return false;
+    return Object.keys(a).every(k => isEqualAdvances(a[k], b[k])) && Object.keys(a).every(i => Object.keys(b).includes(i));
 }
 
 window.isEqualAdvances = isEqualAdvances;
-
 export default isEqualAdvances;

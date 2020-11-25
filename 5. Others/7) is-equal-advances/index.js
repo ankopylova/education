6683d93bed9a634b53ac24372c40abc40798d956
ankopylova <akopylova.excessteam@gmail.com>
2020-11-25 11:31:44 +0300

    /is-equal-advances

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -3,11 +3,6 @@ function isEqualAdvances(a, b) {
     if (!a || !b || typeof a !== 'object' || typeof b !== 'object') return a === b;
     if (a === undefined || b === undefined || a === null || b === null)
         return a === b
-    if (Object.keys(a).values() === undefined || Object.keys(b).values() === undefined
-        || Object.keys(a).values() === null
-        || Object.keys(b).values() === null) {
-        return Object.keys(a).values() === Object.keys(b).values()
-    }
     if (Object.keys(a).length !== Object.keys(b).length) return false;
     return Object.keys(a).every(k => isEqualAdvances(a[k], b[k])) && Object.keys(a).every(i => Object.keys(b).includes(i));
 }

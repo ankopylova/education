5b42c7e61090a9a1fdf9942d132dbde8df7ddfab
ankopylova <akopylova.excessteam@gmail.com>
2020-12-04 18:08:51 +0300

    /token-service

--- 5. Others/5) token-service/index.js
+++ 5. Others/5) token-service/index.js
@@ -1,26 +1,15 @@
+/*
+  Задача
+  Написать TokenService, в котором есть слудеющие функции
+  subscribe(callback) - добавляет подписку на изменение токена (параметром будет новый токен, или null, если токен удален)
+  setToken(token) - устанавливает значения токена (вызывает подписчиков)
+  removeToken() - удаляет токен (вызвает подписчиков на изменеия)
+  getToken() - возващяет токен
+ */
+
 class TokenService {
-    constructor(token) {
-        token = ""
-        this.token = token;
-    }
-
-    subscribe(data) {
-
-    }
-
-    setToken() {
-
-    }
-
-    removeToken() {
-
-    }
-
-    getToken() {
-        return this.token
-    }
+    // Напиши свой код здесь
 }
-
 window.TokenService = TokenService;
 
 export default TokenService;
